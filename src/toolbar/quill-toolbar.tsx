1e57f6016ce23b318ba3b26b411cbbc4e16807d4
Bela Berentes <bela.berentes@motivosity.com>
2023-02-28 23:36:14 +0100

    Add optional separator prop.
    Update package.json. Some clean up

--- src/toolbar/quill-toolbar.tsx
+++ src/toolbar/quill-toolbar.tsx
@@ -22,7 +22,7 @@ import type QuillEditor from '../editor/quill-editor';
 import { ToolbarProvider } from './components/toolbar-context';
 import { SelectionBar } from './components/selection-bar';
 import { ToolSet } from './components/tool-set';
-import { ToolbarSeperator } from './components/toolbar-separator';
+import { ToolbarSeparator } from './components/toolbar-separator';
 import type { FormatChangeData } from '../constants/editor-event';
 
 const WIDTH = Dimensions.get('window').width;
@@ -32,6 +32,7 @@ interface QuillToolbarProps {
   styles?: CustomStyles;
   editor: React.RefObject<QuillEditor>;
   theme: ToolbarTheme | 'dark' | 'light';
+  separator?: boolean;
   custom?: ToolbarCustom;
   container?: false | 'avoiding-view' | React.ComponentType;
 }
@@ -46,6 +47,7 @@ interface ToolbarState {
 export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
   public static defaultProps = {
     theme: 'dark',
+    separator: false,
   };
 
   constructor(props: QuillToolbarProps) {
@@ -92,7 +94,7 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
   private prepareIconset = () => {
     const { options, custom } = this.props;
     let toolbarOptions: Array<Array<string | object> | string | object> = [];
-    if (options === 'full' || options === []) {
+    if (options === 'full') {
       toolbarOptions = fullOptions;
     } else if (options === 'basic') {
       toolbarOptions = basicOptions;
@@ -133,7 +135,7 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
   };
 
   renderToolbar = () => {
-    const { styles, custom } = this.props;
+    const { styles, custom, separator } = this.props;
     const { toolSets, theme, formats } = this.state;
     const defaultStyles = makeStyles(theme);
 
@@ -160,8 +162,8 @@ export class QuillToolbar extends Component<QuillToolbarProps, ToolbarState> {
                 object.length > 0 && (
                   <React.Fragment key={index}>
                     <ToolSet tools={object} />
-                    {toolSets.length > index && (
-                      <ToolbarSeperator color={theme.color} />
+                    {separator && toolSets.length > index && (
+                      <ToolbarSeparator color={theme.color} />
                     )}
                   </React.Fragment>
                 )
